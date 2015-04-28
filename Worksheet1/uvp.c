3d02deda6c8101a193707a63f28b59ef2d846bc0
ludcila <cheunglucia@gmail.com>
2015-04-28 17:10:58 +0200

    Initial implementation of calculate_fg and calculate_rs

--- Worksheet1/uvp.c
+++ Worksheet1/uvp.c
@@ -1,4 +1,5 @@
 #include "uvp.h"
+#include <math.h>
 
 void calculate_dt(
 	double Re,
@@ -30,7 +31,23 @@ void calculate_fg(
 	double **F,
 	double **G
 ) {
-	/* TODO by Luci */
+	int i, j;
+	for(i = 1; i < imax; i++) {
+		for(j = 1; j < jmax; j++) {			/* TODO: check i and j ranges */
+			F[i][j] = U[i][j] + dt * ( 
+				+ 1/Re*((U[i+1][j]-2*U[i][j]+U[i-1][j])/(dx*dx)) 
+				- 1/dx * ( pow((U[i][j]+U[i+1][j])/2, 2) - pow((U[i-1][j]+U[i][j])/2, 2) ) 
+				- 1/dy * ( (V[i][j]+V[i+1][j])*(U[i][j]+U[i][j+1])/4 - (V[i][j-1]+V[i+1][j-1])*(U[i][j-1]+U[i][j])/4 )
+				+ alpha/dy * ( abs(V[i][j]+V[i+1][j])*(U[i][j]+U[i][j+1])/4 - abs(V[i][j-1]+V[i+1][j-1])*(U[i][j-1]+U[i][j])/4 )
+			);
+			G[i][j] = V[i][j] + dt * ( 
+				+ 1/Re*((V[i][j+1]-2*V[i][j]+V[i][j-1])/(dy*dy)) 
+				- 1/dy * ( pow((V[i][j]+V[i][j+1])/2, 2) - pow((V[i][j-1]+V[i][j])/2, 2) ) 
+				- 1/dx * ( (U[i][j]+U[i][j+1])*(V[i][j]+V[i+1][j])/4 - (U[i-1][j]+U[i-1][j+1])*(V[i-1][j]+V[i][j])/4 )
+				- alpha/dx * ( abs(U[i][j]+U[i][j+1])*(V[i][j]+V[i+1][j])/4 - abs(U[i-1][j]+U[i-1][j+1])*(V[i-1][j]+V[i][j])/4 )
+			);
+		}
+	}
 }
 
 
@@ -44,7 +61,12 @@ void calculate_rs(
 	double **G,
 	double **RS
 ) {
-	/* TODO by Luci */
+	int i, j;
+	for(i = 0; i < imax; i++) {
+		for(j = 0; j < jmax; j++) {			/* TODO: check i and j ranges */
+			RS[i][j] = 1/dt * ((F[i][j]-F[i-1][j])/dx + (G[i][j]-G[i][j-1])/dy);
+		}
+	}
 }
 
 
