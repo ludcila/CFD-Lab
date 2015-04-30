f4db5fc3fe4ec7bcbbf3ade124775adc6ed9d5ce
ludcila <cheunglucia@gmail.com>
2015-04-30 17:51:41 +0200

    Change indices and write output file

--- Worksheet1/main.c
+++ Worksheet1/main.c
@@ -52,11 +52,11 @@ int main(int argn, char** args){
 
 
 	/* - set up the matrices (arrays) needed using the matrix() command */
-	U =matrix(0, imax+1, 0, jmax+1);
-	V =matrix(0, imax+1, 0, jmax+1);
+	U =matrix(0, imax  , 0, jmax+1);
+	V =matrix(0, imax+1, 0, jmax  );
 	P =matrix(0, imax+1, 0, jmax+1);
-	F =matrix(0, imax+1, 0, jmax+1); /*G,F,RS domain checks in need*/
-	G =matrix(0, imax+1, 0, jmax+1);
+	F =matrix(0, imax  , 0, jmax+1); /*G,F,RS domain checks in need*/
+	G =matrix(0, imax+1, 0, jmax  );
 	RS=matrix(0, imax+1, 0, jmax+1);
 	/* - create the initial setup init_uvp(), init_flag(), output_uvp() */
 	init_uvp(UI,VI,PI,imax,jmax, U, V, P);
@@ -83,13 +83,13 @@ while(t < t_end){
 	n++;
 	
 }
+
 	free_matrix(U, 0, 0, imax+1, jmax+1);
 	free_matrix(V, 0, 0, imax+1, jmax+1);
 	free_matrix(P, 0, 0, imax+1, jmax+1);
 	free_matrix(F, 0, 0, imax+1, jmax+1);
 	free_matrix(G, 0, 0, imax+1, jmax+1);
 	free_matrix(RS, 0, 0, imax+1, jmax+1);
-
 
 	/*Output of u, v, p for visualization*/
 	write_vtkFile("test", n, xlength, ylength, imax, jmax, dx, dy, U, V, P);
