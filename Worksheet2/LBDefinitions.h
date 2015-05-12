f71c802cfde3bc12ebf64564a97c68cac23db8dd
ludcila <cheunglucia@gmail.com>
2015-05-12 15:25:07 +0200

    Add C_S definition

--- Worksheet2/LBDefinitions.h
+++ Worksheet2/LBDefinitions.h
@@ -1,9 +1,11 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
+#include <math.h>
 #define Q 		19
 #define D 		3
 #define SQRT2 	1.41421356237309504880
+#define SQRT3	1.73205080756887729353
 
 	static const int LATTICEVELOCITIES[19][3] = {
 	
@@ -31,6 +33,7 @@
 		{ 0,  1,  1}
 		
 	};
+	
 	static const double LATTICEWEIGHTS[19] = {
 	
 		SQRT2,
@@ -57,7 +60,8 @@
 		SQRT2		
 		
 	};
-	static const double C_S;
+	
+	static const double C_S = 1.0 / SQRT3;
 
 #endif
 
