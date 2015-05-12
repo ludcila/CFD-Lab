1594cbea43830e2157f297a6ba8db27d5d7e610f
ludcila <cheunglucia@gmail.com>
2015-05-12 14:40:00 +0200

    Add constant definitions

--- Worksheet2/LBDefinitions.h
+++ Worksheet2/LBDefinitions.h
@@ -1,9 +1,63 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
-  static const int LATTICEVELOCITIES[19][3];
-  static const double LATTICEWEIGHTS[19];
-  static const double C_S;
+#define Q 		19
+#define D 		3
+#define SQRT2 	1.41421356237309504880
+
+	static const int LATTICEVELOCITIES[19][3] = {
+	
+		{ 0, -1, -1},
+		{-1,  0, -1},
+		{ 0,  0, -1},
+		{ 1,  0, -1},
+		{ 0,  1, -1},
+		
+		{-1, -1,  0},
+		{ 0, -1,  0},
+		{ 1, -1,  0},
+		{-1,  0,  0},
+		{ 0,  0,  0},
+		
+		{ 1,  0,  0},
+		{-1,  1,  0},
+		{ 0,  1,  0},
+		{ 1,  1,  0},
+		{ 0, -1,  1},
+		
+		{-1,  0,  1},
+		{ 0,  0,  1},
+		{ 1,  0,  1},
+		{ 0,  1,  1}
+		
+	};
+	static const double LATTICEWEIGHTS[19] = {
+	
+		SQRT2,
+		SQRT2,
+		1,
+		SQRT2,
+		SQRT2,
+		
+		SQRT2,
+		1,
+		SQRT2,
+		1,
+		0,
+		
+		1,
+		SQRT2,
+		1,
+		SQRT2,
+		SQRT2,
+		
+		SQRT2,
+		1,
+		SQRT2,
+		SQRT2		
+		
+	};
+	static const double C_S;
 
 #endif
 
