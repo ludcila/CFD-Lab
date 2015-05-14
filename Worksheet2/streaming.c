f863c7efc19ed1caae22409239b813a460f972b1
sanjuu11525 <sanjuu11525@hotmail.com>
2015-05-14 15:37:38 +0200

    add streaming.c

--- Worksheet2/streaming.c
+++ Worksheet2/streaming.c
@@ -1,6 +1,21 @@
 #include "streaming.h"
+#include "LBDefinitions.h"
+#include <stdlib.h>
 
 void doStreaming(double *collideField, double *streamField,int *flagField,int xlength){
-  /* TODO */
+  	int x,y,z;
+	int i;
+
+	for(z=1;z<=xlength;z++){
+		for(y=1;y<=xlength;y++){
+			for(x=1;x<=xlength;x++){
+				if(flagField[(z*xlength*xlength+y*xlength+x)]==0){
+					for(i=0;i<Q;i++){
+						streamField[Q*(z*xlength*xlength+y*xlength+x)+i]=collideField[Q*((-LATTICEVELOCITIES[i][1]+z)*xlength*xlength+(-LATTICEVELOCITIES[i][2]+y)*xlength+(-LATTICEVELOCITIES[i][3]+x))+i]; /*need to be checked*/
+							}
+				}
+			}	
+		}
+	}
 }
 
