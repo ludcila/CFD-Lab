e4a53d55935df4254e0fc07485a4c065f98c016e
sanjuu11525 <sanjuu11525@hotmail.com>
2015-05-15 13:44:47 +0200

    rewrite index for streaming.c

--- Worksheet2/streaming.c
+++ Worksheet2/streaming.c
@@ -9,11 +9,13 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
 	for(z=1;z<=xlength;z++){
 		for(y=1;y<=xlength;y++){
 			for(x=1;x<=xlength;x++){
-				if(flagField[(z*xlength*xlength+y*xlength+x)]==0){
+/*				if(flagField[(z*xlength*xlength+y*xlength+x)]==0){*/
 					for(i=0;i<Q;i++){
-						streamField[Q*(z*xlength*xlength+y*xlength+x)+i]=collideField[Q*((-LATTICEVELOCITIES[i][1]+z)*xlength*xlength+(-LATTICEVELOCITIES[i][2]+y)*xlength+(-LATTICEVELOCITIES[i][3]+x))+i]; /*need to be checked*/
+						streamField[Q*(z*(xlength+1)*(xlength+1)+y*(xlength+1)+x)+i]=collideField
+							[Q*((-LATTICEVELOCITIES[i][1]+z)*(xlength+1)*(xlength+1)+
+							(-LATTICEVELOCITIES[i][2]+y)*(xlength+1)+(-LATTICEVELOCITIES[i][3]+x))+i]; /*need to be checked*/
 							}
-				}
+			/*	}    */
 			}	
 		}
 	}
