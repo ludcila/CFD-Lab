101287f7ec8e9acf51a084a0f1a7c932a35930b0
jsungjei <jsungjei@gmail.com>
2015-05-17 23:04:22 -0700

    revised Boundary and visualLB

--- Worksheet2/boundary.c
+++ Worksheet2/boundary.c
@@ -1,6 +1,40 @@
 #include "boundary.h"
+#include "LBDefinitions.h"
+#include "computeCellValues.h"
+
 
 void treatBoundary(double *collideField, int* flagField, const double * const wallVelocity, int xlength){
-  /* TODO */
+	/* TODO Jae */
+	int x, y, z, i;
+	int cellIdx;
+	double *currentCell;
+	double density;
+	double velocity[3];
+	double c_dot_u;			
+
+	for(z = 1; z <= xlength; z++) {
+		for(y = 1; y <= xlength; y++) {
+			for(x = 1; x <= xlength; x++) {
+				cellIdx = Q * (z * xlength * xlength + y * xlength + x);  
+				currentCell = collideField + cellIdx;
+				
+				computeDensity(currentCell, &density);
+
+				if(*(flagField + cellIdx) == 1)
+				for ( i = 0; i < Q; i++){
+					c_dot_u = LATTICEVELOCITIES[i][0] * velocity[0] + LATTICEVELOCITIES[i][1] * velocity[1] + LATTICEVELOCITIES[i][2] * velocity[2];
+					currentCell[i] = currentCell[i] + 2 * LATTICEWEIGHTS[i] * density * c_dot_u * wallVelocity[1] / (C_S * C_S);				
+				}
+				else if(*(flagField + cellIdx) == 2)
+				for ( i = 0 ; i < Q; i++){
+					currentCell[i] = currentCell[i] + 2 * LATTICEWEIGHTS[i] * density * c_dot_u * wallVelocity[2] / (C_S * C_S);
+					computeDensity(currentCell, &density);
+					currentCell[i] = currentCell[i] + 2 * LATTICEWEIGHTS[i] * density * c_dot_u * wallVelocity[2] / (C_S * C_S);
+					
+				}
+
+			}
+		}
+	}	
 }
 
--- Worksheet2/visualLB.h
+++ Worksheet2/visualLB.h
@@ -5,7 +5,20 @@
  *  to a file determined by 'filename' and timestep 't'. You can re-use parts of the code
  *  from visual.c (VTK output for Navier-Stokes solver) and modify it for 3D datasets.
  */
-void writeVtkOutput(const double * const collideField, const int * const flagField, const char * filename, unsigned int t, int xlength);
+void writeVtkOutput(const double * const collideField, const int * const flagField, const char * filename, unsigned int tau, int xlength);
+
+
+/**
+
+ */
+void write_vtkHeader( FILE *fp, int xlength );
+
+/**
+
+ */
+void write_vtkPointCoordinates( FILE *fp, int xlength, double tau);
+
+
 
 #endif
 
