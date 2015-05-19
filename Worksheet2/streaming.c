c91205469bb7c8e907db518a53a06bf7caac073d
ludcila <cheunglucia@gmail.com>
2015-05-19 14:02:15 +0200

    Fix streaming indices

--- Worksheet2/streaming.c
+++ Worksheet2/streaming.c
@@ -1,21 +1,32 @@
 #include "streaming.h"
 #include "LBDefinitions.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 void doStreaming(double *collideField, double *streamField,int *flagField,int xlength){
   	int x,y,z;
+  	int xx, yy, zz;
 	int i;
+	int cellIdx, neighborCellIdx;
+	double *currentCell, *neighborCell;
+	int numGridPoints = xlength + 2;
 
 	for(z=1;z<=xlength;z++){
 		for(y=1;y<=xlength;y++){
 			for(x=1;x<=xlength;x++){
-/*				if(flagField[(z*xlength*xlength+y*xlength+x)]==0){*/
-					for(i=0;i<Q;i++){
-						streamField[Q*(z*(xlength+1)*(xlength+1)+y*(xlength+1)+x)+i]=collideField
-							[Q*((-LATTICEVELOCITIES[i][1]+z)*(xlength+1)*(xlength+1)+
-							(-LATTICEVELOCITIES[i][2]+y)*(xlength+1)+(-LATTICEVELOCITIES[i][3]+x))+i]; /*need to be checked*/
-							}
-			/*	}    */
+				double sum = 0;
+				cellIdx = Q * (z * numGridPoints * numGridPoints + y * numGridPoints + x);
+				currentCell = streamField + cellIdx;
+				for(i=0;i<Q;i++){
+					xx = x -LATTICEVELOCITIES[i][0];
+					yy = y -LATTICEVELOCITIES[i][1];
+					zz = z -LATTICEVELOCITIES[i][2];
+					neighborCellIdx = Q * (zz * numGridPoints * numGridPoints + yy * numGridPoints + xx);
+					neighborCell = collideField + neighborCellIdx;
+					currentCell[i] = neighborCell[i];
+					sum += currentCell[i];
+				}
+				/*printf("%f ", sum);*/
 			}	
 		}
 	}
