f104c0c566d05c78fde7a1aa9af0ff18f0ed6ae5
ludcila <cheunglucia@gmail.com>
2015-05-30 00:45:18 +0200

    Modify SOR

--- Worksheet3/sor.c
+++ Worksheet3/sor.c
@@ -13,7 +13,7 @@ void sor(
   int **Flag
 ) {
   int i,j;
-  int count_num_fluid_cell;		/* count neighboring fluid cells */
+  int count_num_fluid_cell = 0;
   double P_N, P_S, P_W, P_O;
   double rloc;
   double coeff = omg/(2.0*(1.0/(dx*dx)+1.0/(dy*dy)));
@@ -30,11 +30,14 @@ void sor(
   rloc = 0;
   for(i = 1; i <= imax; i++) {
     for(j = 1; j <= jmax; j++) {
-      rloc += ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j])*
-              ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j]);
+    	if(Flag[i][j] & 16) {
+		  rloc += ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j])*
+		          ( (P[i+1][j]-2.0*P[i][j]+P[i-1][j])/(dx*dx) + ( P[i][j+1]-2.0*P[i][j]+P[i][j-1])/(dy*dy) - RS[i][j]);
+		  count_num_fluid_cell++;
+        }
     }
   }
-  rloc = rloc/(imax*jmax);
+  rloc = rloc / count_num_fluid_cell;
   rloc = sqrt(rloc);
   /* set residual */
   *res = rloc;
@@ -53,34 +56,33 @@ void sor(
   /* compute pressures of obstacle cells */
 	for(i = 1; i <= imax; i++) {
 		for(j = 1; j<=jmax; j++) {
-			if(Flag[i][j] && 1){ /* B_N, north cell is fluid */
-				P_N = P[i][j+1];
-				count_num_fluid_cell++;
-			} 
-
-			if(Flag[i][j] && 2){ /* B_S, south cell is fluid */
-				P_N = P[i][j-1];
-				count_num_fluid_cell++;
-			}
-
-			if(Flag[i][j] && 4){ /* B_W, west cell is fluid */
-				P_N = P[i-1][j];
-				count_num_fluid_cell++;
-			}
-
-			if(Flag[i][j] && 8){ /* B_O, east cell is fluid */
-				P_N = P[i+1][j];
-				count_num_fluid_cell++;
+			switch(Flag[i][j]) {
+				case B_N:
+					P[i][j] = P[i][j+1];
+					break;
+				case B_S:
+					P[i][j] = P[i][j-1];
+					break;
+				case B_O:
+					P[i][j] = P[i+1][j];
+					break;
+				case B_W:
+					P[i][j] = P[i-1][j];
+					break;
+				case B_NW:
+					P[i][j] = 0.5 * (P[i-1][j] + P[i][j+1]);
+					break;
+				case B_NO:
+					P[i][j] = 0.5 * (P[i+1][j] + P[i][j+1]);
+					break;
+				case B_SW:
+					P[i][j] = 0.5 * (P[i-1][j] + P[i][j-1]);
+					break;
+				case B_SO:
+					P[i][j] = 0.5 * (P[i+1][j] + P[i][j-1]);
+					break;
 			}
 		}
-
-		P[i][j] = (P_N + P_S + P_W + P_O) / count_num_fluid_cell;
-
-		P_N = 0;
-		P_S = 0;
-		P_W = 0;
-		P_O = 0;
-		count_num_fluid_cell = 0;
 	}
 
 }
