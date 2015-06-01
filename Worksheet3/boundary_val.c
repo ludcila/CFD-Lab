88932274366b3c9b0aafa39ea229c40ccefa3b18
jsungjei <jsungjei@gmail.com>
2015-06-01 11:50:52 -0700

    dp_ver_3

--- Worksheet3/boundary_val.c
+++ Worksheet3/boundary_val.c
@@ -212,7 +212,6 @@ void spec_boundary_val (char *problem, int imax, int jmax, double **U, double **
 					V[0][j] = 0;
 				}
 				V[0][j+1] = 0;
-			}
 		} else if(strcmp(problem, "karman_vortex_street") == 0) {
 			for(j = 0; j <= jmax; j++) {
 				U[0][j] = 1;
