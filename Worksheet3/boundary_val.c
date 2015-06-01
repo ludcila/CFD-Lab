46f727c4df594a59cea3c7f27f6d56ad2041e17f
jsungjei <jsungjei@gmail.com>
2015-06-01 11:55:27 -0700

    dp_ver_4

--- Worksheet3/boundary_val.c
+++ Worksheet3/boundary_val.c
@@ -200,24 +200,10 @@ void spec_boundary_val (char *problem, int imax, int jmax, double **U, double **
 	
 	int j;
 	if(dp == 0){
-		if(strcmp(problem, "flow_over_step") == 0) {
-			for(j = jmax/2 + 1; j <= jmax; j++) {
-				U[0][j] = 1;
-				V[0][j] = 0;
-			}
-			V[0][jmax+1] = 0;
-		}else if(strcmp(problem, "karman_vortex_street") == 0) {
-				for(j = 0; j <= jmax; j++) {
-					U[0][j] = 1;
-					V[0][j] = 0;
-				}
-				V[0][j+1] = 0;
-		} else if(strcmp(problem, "karman_vortex_street") == 0) {
-			for(j = 0; j <= jmax; j++) {
-				U[0][j] = 1;
-				V[0][j] = 0;
-			}
-			V[0][j+1] = 0;
+		for(j = 0; j <= jmax; j++) {
+			U[0][j] = 1;
+			V[0][j] = 0;
 		}
+		V[0][j+1] = 0;
 	}
 }
